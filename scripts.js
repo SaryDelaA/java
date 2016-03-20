b2d15bfaec789e7e934db086c616ad26ab9bff9a
SaryDelaA <sarycdo@gmail.com>
2016-03-20 15:03:39 -0500

    errores

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
+console.log("Inicio de programa");
+
 alert("Hola JavaScript");
 
-document.write("<h1>Hola JavaScript :P</h1>");
+document.write("<h1>Hola JavaScript :P </h1>");
+
+alert("Gracias por visitarnos");
+
+console.log("fin de programa");
