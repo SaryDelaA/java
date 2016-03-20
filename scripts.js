d2ee0700d8b1d1638d92423c927cd317ee0e56ec
SaryDelaA <sarycdo@gmail.com>
2016-03-20 16:57:24 -0500

    agregado mensajes

--- scripts.js
+++ scripts.js
@@ -1,5 +1,14 @@
-console.log("Inicio de programa");
+/*console.log("Inicio de programa");
 alert("Hola JavaScript");
 document.write("<h1>Hola JavaScript :P </h1>");
 alert("Gracias por visitarnos");
-console.log("fin de programa");
+console.log("fin de programa");*/
+
+/*var equipo=prompt("Cual es tu equipo de futbol favorito");
+document.write("Mi equipo favorito es  " +equipo );*/
+
+/*METODOS Y PROPIEDADES*/
+var clave=prompt ("Hola Usuario, Ingrese su clave");
+console.log("La longitug de la clave es:" + clave.length);
+console.log("TA-DA!!! Su clave es: "+ clave.toUpperCase());
+console.log("El valor original de la clave es:" +clave);
