f955b2012230000b264e7bd8df5acd2bed47d697
SaryDelaA <sarycdo@gmail.com>
2016-03-20 15:14:07 -0500

    con errores

--- scripts.js
+++ scripts.js
@@ -1,9 +1,7 @@
-console.log("Inicio de programa");
-
+console.log("Inicio de programa";
 alert("Hola JavaScript");
-
 document.write("<h1>Hola JavaScript :P </h1>");
-
+document.write("<h1>mundito "</h1>)
 alert("Gracias por visitarnos");
-
+console.("programa alterno")
 console.log("fin de programa");
